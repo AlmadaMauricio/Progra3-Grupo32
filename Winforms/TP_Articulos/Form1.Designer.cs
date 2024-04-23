4d0c5401961a324be264328f05a96be8be11ad33
Juan Cruz <juan.escalante@alumnos.frgp.utn.edu.ar>
2024-04-22 23:13:28 -0300

    Primer Subida
    Se crearon las clases y la conexión a la base de datos. Por último, se sumó el dgv para verificar que la conexión está funcionando.

---
+++ Winforms/TP_Articulos/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace TP_Articulos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAtrticulos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAtrticulos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAtrticulos
+            // 
+            this.dgvAtrticulos.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvAtrticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAtrticulos.Location = new System.Drawing.Point(35, 50);
+            this.dgvAtrticulos.Name = "dgvAtrticulos";
+            this.dgvAtrticulos.RowHeadersWidth = 51;
+            this.dgvAtrticulos.RowTemplate.Height = 24;
+            this.dgvAtrticulos.Size = new System.Drawing.Size(555, 290);
+            this.dgvAtrticulos.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvAtrticulos);
+            this.Name = "Form1";
+            this.Text = "Articulos";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAtrticulos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAtrticulos;
+    }
+}
+
